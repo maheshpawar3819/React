be6f08aa7a65bcfc5f9870ef11c7a8480f2de3dc
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-11 17:09:58 +0530

    modify

--- practice 1/src/App.jsx
+++ practice 1/src/App.jsx
@@ -2,17 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './comonents/Home'
-import Header from './comonents/Header'
-import Footer from './comonents/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
       <Header/>
+      <Home/>
       <Footer/>
     </>
   )
