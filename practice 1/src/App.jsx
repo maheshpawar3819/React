5c140eb8f5512a25c7ab6abf00dd4df3fc3d3433
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-11 16:49:45 +0530

    modify

--- practice 1/src/App.jsx
+++ practice 1/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './comonents/Home'
+import Header from './comonents/Header'
+import Footer from './comonents/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,8 @@ function App() {
   return (
     <>
       <Home/>
+      <Header/>
+      <Footer/>
     </>
   )
 }
