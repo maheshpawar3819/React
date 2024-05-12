9e2fad6f4e6834747369d662f1ca0993f1092aee
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-12 13:50:24 +0530

    modify files

--- practice 1/src/comonents/Header.jsx
+++ practice 1/src/comonents/Header.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      <h1>hii mahi
-        
-      </h1>
+    <div className=''>
+        <nav className='d-flex align-center justify-content-between ms-10 bg-y'>
+        <li className='list-unstyled me-5 fs-5 text-dark'>
+            Home
+        </li>
+        <li className='list-unstyled me-5 fs-5 text-dark'>
+            Product
+        </li>
+        <li className='list-unstyled me-5 fs-5 text-dark'>
+            Contact Us
+        </li>
+        <li className='list-unstyled me-5 fs-5 text-dark'>
+            About Us
+        </li>
+        </nav>
     </div>
   )
 }
