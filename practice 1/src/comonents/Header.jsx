15ac2026c68fbf0ebdd07a0987923704ddf46294
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-12 14:06:55 +0530

    add header module create nav bar

--- practice 1/src/comonents/Header.jsx
+++ practice 1/src/comonents/Header.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=''>
-        <nav className='d-flex align-center justify-content-between ms-10 bg-y'>
-        <li className='list-unstyled me-5 fs-5 text-dark'>
+    <div className='col' >
+        <nav className='d-flex align-center justify-content-between ms-5 bg-light p-2'>
+        <li className='list-unstyled me-5 fs-5 text-dark ms-5'>
             Home
         </li>
-        <li className='list-unstyled me-5 fs-5 text-dark'>
+        <li className='list-unstyled me-5 fs-5 text-dark ms-5'>
             Product
         </li>
-        <li className='list-unstyled me-5 fs-5 text-dark'>
+        <li className='list-unstyled me-5 fs-5 text-dark ms-5'>
             Contact Us
         </li>
-        <li className='list-unstyled me-5 fs-5 text-dark'>
+        <li className='list-unstyled me-5 fs-5 text-dark ms-5'>
             About Us
         </li>
         </nav>
