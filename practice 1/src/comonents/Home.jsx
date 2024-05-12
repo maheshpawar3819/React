19fe24730afe200fc7171e5147b7f4eeb161d61a
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-12 14:33:10 +0530

    add banner image

---
+++ practice 1/src/comonents/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <img src="https://media.istockphoto.com/id/1308634004/photo/focus-on-the-laptop-keyboard-with-blurred-library-background.webp?b=1&s=170667a&w=0&k=20&c=G1NTsOAJ2Eek7SfNRokCmDK3ABevGYsVaiHuFWaMSR4=" alt=""  className="rounded" style={{width:"50vw"}}/>
+    </div>
+  )
+}
+
+export default Home
