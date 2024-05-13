2748c58abe2259de6224d8808a9bb483f4ced48a
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-13 12:13:54 +0530

    final project countnum.jsx

--- practice 1/src/comonents/Countnum.jsx
+++ practice 1/src/comonents/Countnum.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import { useState } from "react";
 
 const Countnum = () => {
-  const [state, setState] = useState("hii mahi");
+  const [state, setState] = useState(0);
 
-  function Increment(num){
-    setState(num++);
-    
+  function Increment() {
+    setState((num) => num + 1);
   }
 
-  function Decrement(num){
-    setState(num--);
+  function Decrement(num) {
+    setState((num) => num - 1);
   }
   return (
-
     <div>
       <p className="mt-5">{state}</p>
 
-      <button className="btn btn-info m-4" onClick={Increment}>Increment</button>
-      <button className="btn btn-danger" onClick={Decrement}>Decrement</button>
+      <button className="btn btn-info m-4" onClick={Increment}>
+        Increment
+      </button>
+      <button className="btn btn-danger" onClick={Decrement}>
+        Decrement
+      </button>
     </div>
   );
 };
