8257badd374d5581d5b79fb23fe5f9aa2934f93f
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-15 12:14:33 +0530

    create indivisual files app.js and index.css

---
+++ Namaste React/app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h2", { id: "heading" }, "Hii Mahi!!"); //in the object you can give attributes to your html tag.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
