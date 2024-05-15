b565a62416bcf1fd232cca98da6fc2ae31e359e2
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-15 15:40:19 +0530

    add listtable.jsx file

--- practice 1/src/App.jsx
+++ practice 1/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Calculetor from './comonents/Calculetor'
+import Listtable from './comonents/Listtable'
 
 
 
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-     <Calculetor/>
+     <Listtable/>
     </>
   )
 }
---
+++ practice 1/src/comonents/Listtable.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+const Listtable = () => {
+  const list = [
+    {
+      name: "mahi",
+      standard: "BCA",
+      age: 21,
+      marks: 90,
+    },
+    {
+      name: "kunal",
+      standard: "BCA",
+      age: 21,
+      marks: 80,
+    },
+    {
+      name: "soham",
+      standard: "BCA",
+      age: 21,
+      marks: 50,
+    },
+    {
+      name: "ashwini",
+      standard: "BCA",
+      age: 21,
+      marks: 70,
+    },
+    {
+      name: "saurabh",
+      standard: "BCA",
+      age: 21,
+      marks: 60,
+    },
+    {
+      name: "neha",
+      standard: "BCA",
+      age: 21,
+      marks: 70,
+    },
+    {
+      name: "pratiksha",
+      standard: "BCA",
+      age: 21,
+      marks: 40,
+    },
+    {
+      name: "anjali",
+      standard: "BCA",
+      age: 21,
+      marks: 80,
+    },
+    {
+      name: "rahul",
+      standard: "BCA",
+      age: 21,
+      marks: 90,
+    },
+    {
+      name: "sneha",
+      standard: "BCA",
+      age: 21,
+      marks: 100,
+    },
+  ];
+  return (
+    <div>
+      {list.map((ele) => {
+        return (
+          <div>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Standard</th>
+                  <th scope="col">Age</th>
+                  <th scope="col">Marks</th>
+                  <tr>
+                  <td>{ele.name}</td>
+                  <td>{ele.standard}</td>
+                  <td>{ele.age}</td>
+                  <td>{ele.marks}</td>
+                </tr>
+                </tr>
+              </thead>
+              <tbody>
+                
+              </tbody>
+            </table>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Listtable;
