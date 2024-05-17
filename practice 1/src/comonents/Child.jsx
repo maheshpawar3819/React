a449a7a2774f3b483b487d5cfead27de238bcb75
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-17 14:06:45 +0530

    add more items

--- practice 1/src/comonents/Child.jsx
+++ practice 1/src/comonents/Child.jsx
@@ -8,9 +8,14 @@ const Child = (props) => {
     }
   return (
     <div>
-      <button onClick={forbtn}>
-        Add 
+        <h1 className='offset-2 text-secondary'>Add Numbers</h1>
+        <input type="text" className='form-control offset-1 mt-5' />
+        <input type="text" className='form-control offset-1 mt-5' />
+
+      <button className='btn btn-danger mt-4 offset-2 ps-4 pe-4 p-2'>
+        Add
       </button>
+        
     </div>
   )
 }
