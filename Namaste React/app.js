c009aca19775ce225b6e3304710ab0c1ed2af276
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-27 14:26:15 +0530

    final body component

--- Namaste React/app.js
+++ Namaste React/app.js
@@ -51,21 +51,20 @@ const Header = () => {
 const Restaurentcard = (props) => {
   //destructuring obj
 
-  const { restaurent } = props;
+
+  const { img, rtname, Ratings, menu, location } = props.restaurent;
 
   return (
-    
-      <div className="card-container">
-        <img src={restaurent.img} className="rt-img" />
-        <h3 className="card-head">{restaurent.rtname}</h3>
-        <p id="str-rating">
-          <span className="rating">{restaurent.Ratings}</span>
-        </p>
-        <p>{restaurent.menu}</p>
+    <div className="card-container">
+      <img src={img} className="rt-img" />
+      <h3 className="card-head">{rtname}</h3>
+      <p id="str-rating">
+        <span className="rating">{Ratings}</span>
+      </p>
+      <p>{menu}</p>
 
-        <p>{restaurent.location}</p>
-      </div>
-
+      <p>{location}</p>
+    </div>
   );
 };
 
@@ -78,66 +77,66 @@ const data = [
     location: "Phaltan",
   },
   {
-    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kyazotklprl8in3rlzmf",
-    rtname: "Arbain Mandi",
-    Ratings: "4.5 30-40 Min",
-    menu: "North indian biryani",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ghlqqsuu7ovhuzq8txgu",
+    rtname: "Pind Punjabi",
+    Ratings: "4.2 30-35 Min",
+    menu: "Punjabi,north indian",
+    location: "Baramati",
+  },
+  {
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/pxkczssapfhhd8w3b9jd",
+    rtname: "Punjabi Treat",
+    Ratings: "4.1 30-40 Min",
+    menu: "Chainese North Indian",
     location: "Phaltan",
   },
   {
-    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kyazotklprl8in3rlzmf",
-    rtname: "Arbain Mandi",
-    Ratings: "4.5 30-40 Min",
-    menu: "North indian biryani",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/tsw3vukgewwlhmqni762",
+    rtname: "Godavari Ruchulu",
+    Ratings: "3.8 30-40 Min",
+    menu: "Sauth indian biryani",
+    location: "Baramati",
+  },
+  {
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rb7lzh9ivotudnzr9rct",
+    rtname: "Hotel Chaitantya",
+    Ratings: "4.2 30-40 Min",
+    menu: "Maharashtrian Chainese",
+    location: "Baramati",
+  },
+  {
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/d2b39bda3a57f79ddf74dc0a2f4eb37c",
+    rtname: "Biryani Zone",
+    Ratings: "4 30-40 Min",
+    menu: "Biryani",
     location: "Phaltan",
   },
   {
-    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kyazotklprl8in3rlzmf",
-    rtname: "Arbain Mandi",
-    Ratings: "4.5 30-40 Min",
-    menu: "North indian biryani",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/wkmehhokjp57lup8ij1s",
+    rtname: "Biryani By KG",
+    Ratings: "3.9 30-40 Min",
+    menu: "North indian Hydrabadi biryani",
+    location: "Baramati",
+  },
+  {
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/wkmehhokjp57lup8ij1s",
+    rtname: "Behrouz Biryani",
+    Ratings: "4.3 30-40 Min",
+    menu: "North indian biryani kababs",
+    location: "Baramati",
+  },
+  {
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/k5jwalqkha9t1czl0cc7",
+    rtname: "Aroma Tandoor & Biryani",
+    Ratings: "2.5 30-40 Min",
+    menu: "indian biryani",
     location: "Phaltan",
   },
   {
-    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kyazotklprl8in3rlzmf",
-    rtname: "Arbain Mandi",
-    Ratings: "4.5 30-40 Min",
-    menu: "North indian biryani",
-    location: "Phaltan",
-  },
-  {
-    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kyazotklprl8in3rlzmf",
-    rtname: "Arbain Mandi",
-    Ratings: "4.5 30-40 Min",
-    menu: "North indian biryani",
-    location: "Phaltan",
-  },
-  {
-    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kyazotklprl8in3rlzmf",
-    rtname: "Arbain Mandi",
-    Ratings: "4.5 30-40 Min",
-    menu: "North indian biryani",
-    location: "Phaltan",
-  },
-  {
-    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kyazotklprl8in3rlzmf",
-    rtname: "Arbain Mandi",
-    Ratings: "4.5 30-40 Min",
-    menu: "North indian biryani",
-    location: "Phaltan",
-  },
-  {
-    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kyazotklprl8in3rlzmf",
-    rtname: "Arbain Mandi",
-    Ratings: "4.5 30-40 Min",
-    menu: "North indian biryani",
-    location: "Phaltan",
-  },
-  {
-    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kyazotklprl8in3rlzmf",
-    rtname: "Arbain Mandi",
-    Ratings: "4.5 30-40 Min",
-    menu: "North indian biryani",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/qfcxq8z12q07krpz25ec",
+    rtname: "Tandoor Point",
+    Ratings: "2.5 30-40 Min",
+    menu: "Tandoor",
     location: "Phaltan",
   },
 ];
