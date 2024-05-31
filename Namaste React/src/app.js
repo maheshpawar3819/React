8c410c5e3472c6d3088de80d934bfda70ccfa3d7
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-31 17:52:53 +0530

    add files to practie namaste react

--- Namaste React/src/app.js
+++ Namaste React/src/app.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "./Components/Header";
 
 
 const App = () => {
   return (
     <>
-      
+   <Header/>
     </>
   );
 };
