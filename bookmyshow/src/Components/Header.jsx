558adf92d89028df4afcbdf81d7162e594a63e18
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-07 14:48:36 +0530

    add the header

--- bookmyshow/src/Components/Header.jsx
+++ bookmyshow/src/Components/Header.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div className=''>
-      <nav className='border border-dark d-flex d-flex justify-content-between ms-5 me-5 h-25'>
-        <div className='h-'>
-            <img src="https://asset.brandfetch.io/id4J58sqa_/id3secYmRE.svg" className="" style={{width:"150px", height:"auto"}}alt="logo img" />
+    <div className="">
+      <nav className="border border-dark d-flex d-flex justify-content-between ms-5 me-5 h-25">
+        <div className="h-">
+          <img
+            src="https://asset.brandfetch.io/id4J58sqa_/id3secYmRE.svg"
+            className=""
+            style={{ width: "150px", height: "auto" }}
+            alt="logo img"
+          />
         </div>
-        <div className='border border-dark p-5'>
-
-        </div>
-        <div className='border border-dark p-5'></div>
-        <div className='border border-dark p-5'></div>
+        <div className="border border-dark p-5"></div>
+        <div className="border border-dark p-5"></div>
+        <div className="border border-dark p-5"></div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
