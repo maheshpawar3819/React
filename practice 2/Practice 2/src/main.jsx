7686195aed8a822fa1d60512ab0ecb3901e6fb32
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-11 10:41:47 +0530

    update readme.md

--- practice 2/Practice 2/src/main.jsx
+++ practice 2/Practice 2/src/main.jsx
@@ -1,37 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Contact from "./Components/Contact.jsx";
-import Home from "./Components/Home.jsx";
-import Aboutus from "./Components/Aboutus.jsx";
-import Owner from "./Components/Owner.jsx";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/Contact",
-    element: <Contact />,
-  },
-  {
-    path: "/Aboutus",
-    element: <Aboutus />,
-    children: [
-      {
-        path: "Owner",
-        element: <Owner />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>
 );
