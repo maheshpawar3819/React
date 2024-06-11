1b633c4c374a4c984a23a7ff2a1e75252141df9c
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-11 19:47:53 +0530

    add file shimmer ui

---
+++ practice 2/Practice 2/src/Components/Shimmer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <div className='s-div'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Shimmer
