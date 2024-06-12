d5c2f2887f2643bb4b5b4c4691a7f2242e34fa26
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-12 09:55:17 +0530

    add simple header component

---
+++ Practice 3 (class based Components)/class based/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return <>
+        <header>
+            <h1 style={{textAlign:"center"}}>I am a Header of this project</h1>
+        </header>
+    </>;
+  }
+}
+
+export default Header;
