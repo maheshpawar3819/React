010cb2dba82ad75a1c709dfd8f26be3e45d9dea3
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-12 14:58:13 +0530

    add imp comments about multiple child components mounting in class based component

--- Practice 3 (class based Components)/class based/src/Components/Body.jsx
+++ Practice 3 (class based Components)/class based/src/Components/Body.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import Card from "./Card";
 class Body extends React.Component {
-  constructor(){
-    super()
-    {console.log("parent-const")}
+  constructor() {
+    super();
+    {
+      console.log("parent-const");
+    }
   }
 
-componentDidMount(){
-  {console.log("parent-didmt")}
-}
+  componentDidMount() {
+    {
+      console.log("parent-didmt");
+    }
+  }
 
   render() {
     return (
       <>
         <div>
-          {console.log('parent-render')}
+          {console.log("parent-render")}
           <h1>Hii i am body component</h1>
-          <Card name={"Mahesh"} education={"BCA"} location={"Phaltan"} />
+          <Card name={"first"} education={"BCA"} location={"Phaltan"} />
+          <Card name={"second"} education={"MCA"} location={"Pune"} />
         </div>
       </>
     );
@@ -24,3 +29,19 @@ componentDidMount(){
 }
 
 export default Body;
+
+/* 
+-parent constructor
+-parent render
+    -first constructor
+    -first render
+    
+    -second constructor
+    -second render
+
+  <DOM UPDATE IN SINGLE BATCH>
+    -first componentDidMount
+    -second componentDidMount
+
+-parent componentDidMount
+*/
