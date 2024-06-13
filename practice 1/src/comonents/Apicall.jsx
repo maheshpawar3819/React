b82cd5a5c85cdcf1bd706c0f50a68b2e25ebd5ee
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-13 11:12:35 +0530

    fixed bughs

--- practice 1/src/comonents/Apicall.jsx
+++ practice 1/src/comonents/Apicall.jsx
@@ -10,6 +10,7 @@ const Apicall = () => {
       .get(`https://fakestoreapi.com/products`)
       .then((response) => {
         setState(response.data);
+        console.log(response)
       })
       .catch((err) => {
         console.log(err);
@@ -26,10 +27,12 @@ const Apicall = () => {
         Cloth Store
       </h1>
       {state.map((ele) => {
+       
         return (
           <div
+          key={ele.id}
             className="col-4 p-4"
-            style={{ border: "box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" }}
+            style={{ border: "box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px" }}
           >
             <h2>Product {ele.id}</h2>
             <h4 className="text-dark">{ele.title}</h4>
