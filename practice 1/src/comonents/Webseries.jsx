624fe9e58348ed36796f9db09f9d936fdb3def65
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-15 00:07:59 +0530

    add

--- practice 1/src/comonents/Webseries.jsx
+++ practice 1/src/comonents/Webseries.jsx
@@ -1,29 +1,25 @@
-import React from 'react'
-import axios from 'axios'
-import { useEffect } from 'react'
+import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
 const Webseries = () => {
-
-    async function apidata(){
-       try{
-        const data=await axios.get("https://content-jiovoot.voot.com/psapi/voot/v1/voot-web/content/specific/editorial?query=include%3Aa25a331db61a3ebf4ab31692d45c518b&source=CMS&discounting=false&aspectRatio=3x4&responseType=common")
-        console.log(data.data)
-        console.log(data?.data?.result[0]?.line1);
-        console.log(data?.data?.result[0]?.languages[0]);
-        console.log(data?.data?.result[0]?.deeplinkUrl);
-       } catch (error) {
-            console.log("oops error was found",error);
-       }
+  async function apidata() {
+    try {
+      const data = await axios.get(
+        "https://api.mxplayer.in/v1/web/home/tab/a8ac883f2069d71784f4869e2bfe8340?device-density=2&userid=338729d4-08a1-40f4-bf7b-bc15dfac3fa7&platform=com.mxplay.mobile&content-languages=hi,en&pageSize=2&kids-mode-enabled=false"
+      );
+      console.log(data);
+      console.log(data.data?.sections[0]?.items[0]?.id)
+    
+    } catch (error) {
+      console.log("oops error was found", error);
     }
+  }
 
-    useEffect(() =>{
-        apidata();
-    },[]);
+  useEffect(() => {
+    apidata();
+  }, []);
 
-  return (
-    <div>
+  return <div></div>;
+};
 
-    </div>
-  )
-}
-
-export default Webseries
+export default Webseries;
