d2dcc012abfbf0d8032bfe760c517188251b61ad
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-18 14:24:39 +0530

    clear files

--- Namaste React/src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="ft-container">
-      <h2 className="ft-text"> @All Copyright Receved Food Restaurent</h2>
-    </div>
-  );
-};
-
-export default Footer;
--- Namaste React/src/app.js
+++ Namaste React/src/app.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./Components/Header";
-import Body from "./Components/Body";
-import Footer from "./Components/Footer";
+
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Body />
-      <Footer />
+    
     </>
   );
 };
