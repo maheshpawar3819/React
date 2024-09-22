e6fea39b2da7e2213991064e8e3317129546fb3e
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-09-22 19:40:50 +0530

    fetch data from using inbuilt fetch method

--- practice 1/src/App.jsx
+++ practice 1/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
       {/* <Tndbapicall /> */}
       {/* <Demo/> */}
       {/* <ApiSearchmovies /> */}
+      <Apicall2/>
     </>
   );
 }
--- practice 1/src/comonents/Apicall2.jsx
+++ practice 1/src/comonents/Apicall2.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 
 const Apicall2 = () => {
-  return (
-    <div>
-      hii
-    </div>
-  )
-}
+  useEffect(() => {
+    fetchdata();
+  }, []);
 
-export default Apicall2
+  const fetchdata = async () => {
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/1`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("something wrong cannot able to fetch data ");
+    }
+  };
+
+  return <div>hii</div>;
+};
+
+export default Apicall2;
