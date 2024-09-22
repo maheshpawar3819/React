6f8e7f3264912873c3829729ed75b0831be59d4f
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-09-22 19:47:13 +0530

    add useState to show data on ui

--- practice 1/src/comonents/Apicall2.jsx
+++ practice 1/src/comonents/Apicall2.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const Apicall2 = () => {
+  const [data,setData]=useState([]);
+
   useEffect(() => {
     fetchdata();
   }, []);
 
   const fetchdata = async () => {
     try {
-      const response = await fetch(`https://fakestoreapi.com/products/1`);
+      const response = await fetch(`https://fakestoreapi.com/products?limit=5`);
       const data = await response.json();
       console.log(data);
+      setData(data)
     } catch (error) {
       console.error("something wrong cannot able to fetch data ");
     }
   };
 
-  return <div>hii</div>;
+  return <div>
+    {
+      data.map((ele) => {
+        //destructure the data
+        const {id,title,price,description}=ele;
+        return <ul key={id}>
+          <li>
+          {title}
+          </li>
+          <li>
+            {price}
+          </li>
+          <li>
+            {description}
+          </li>
+        </ul>
+      })
+    }
+  </div>;
 };
 
 export default Apicall2;
